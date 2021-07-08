a619cc01fe408fb92ea7242c29c5f44cabb97a86
Josearmandoforerogonzalez <joseforero0310@gmail.com>
2021-07-07 21:13:27 -0500

    repaso clase 7 de julio de 2021

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("hola mundo");
+var numeroUno = 1
+var numeroDos = 2
+var resultado = numeroUno + numeroDos
+alert("suma, resta, multiplicacion y division y presiona f12 para ver la consola")
+"multiplicaion"
+numUno = 15
+numDos = 15
+resultado = numUno * numDos
+
+"division"
+numUno = 10
+numDos = 2
+resultadoDeLaDivision = numUno / numDos
+
+
+"resta"
+numUno = 50
+numDos = 29
+resultadoDeLaResta = numUno - numDos
+
+
+
+nombreCompletoDelProfesor = `i´m Bender Rodriguez`;
+edad = 21;
+aficionado = "futbol";
+soyUnProfesor = false;
+console.log(soyUnProfesor, typeof(soyUnProfesor))
+
+console.log(nombreCompletoDelProfesor, typeof(nombreCompletoDelProfesor))
+
+// document.write se utiliza para imprimir el dato en pantalla mas no solo en la consola
+// document.write(nombreCompletoDelProfesor)
+
